ac30659643c212d502de49e371c76621607d266d
IDGS801-20002144 <herck.hvn@gmail.com>
2023-03-02 16:08:50 -0600

    modificaciones

--- app.py
+++ app.py
@@ -65,6 +65,40 @@ def diccionario():
     return render_template('traductorh.html',form = reg_language, palabra = palabra, idioma = idioma)
 
 
+@app.route('/Resistencias', methods=['GET', 'POST'])
+def diccionario():
+    reg_language = forms.Resistencias(request.form)
+    esp = ""
+    eng = ""
+    opc = ""
+    bus = ""
+    palabra = ""
+    idioma = ""
+    if request.method == 'POST':
+        esp = reg_language.esp.data
+        eng = reg_language.eng.data
+        opc = reg_language.opc.data
+        bus = reg_language.bus.data
+        if esp != None and eng != None:
+            add_dictionary(eng, esp)
+        if opc != None and bus != None:
+            palabra = search_dictionary(opc, bus)
+            if opc == "eng":
+                idioma = "English"
+            else:
+                idioma = "Español"
+    return render_template('traductorh.html',form = reg_language, palabra = palabra, idioma = idioma)
+
+
+
+
+
+
+
+
+
+
+
 def add_dictionary(eng, esp):
     file = open('traduccion.txt','a')
     file.write(eng.lower())
